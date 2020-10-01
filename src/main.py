87efa648626505d537029c3d3098a54136f74502
Darren <darren.tr@Outlook.com>
2020-10-01 15:45:53 +1000

    Working on Menus

--- src/main.py
+++ src/main.py
@@ -2,53 +2,72 @@ from jikanpy import Jikan
 from src.anime import Recommendation
 from src.datahandling import AnimeData
 from src.datahandling import MangaData
+from menus import Menus
 
 Recommendation.greeting()
 print(Recommendation.greeting())
 
 jikan = Jikan()
 
-def test():
-    current_data = MangaData.load()
-    anime = Recommendation("manga")
+def newrec(data, option):
+    current_data = data.load()
+    anime = Recommendation(option)
     current_data.append(anime.clean())
-    MangaData.save(current_data)
+    data.save(current_data)
     for recommendation in current_data:
         for key,value in recommendation.items():
+            print(f"Your Current {option} Recommendations:\n")
             print(key, "\n")
             for atitle in value:
                 for key1,value1 in atitle.items():
                     print(f"{key1}: {value1}")
 
-test()
+
+def viewlist(data, option):
+    alldata = data.load()
+    print(f"Your Current {option} Recommendations:\n")
+    for recommendation in alldata:
+        for key, value in recommendation.items():
+            print(key, "\n")
+            for atitle in value:
+                for key1, value1 in atitle.items():
+                    print(f"{key1}: {value1}")
 
 
-# Menu Message
-# MENU_MSG = """
-# *****=====*****=====*****=====*****=====*****
-#
-# Would you like to:
-# 1. Get a New Anime Recommendation
-# 2. Get a New Manga Recommendation
-# 3. View Your Anime Recommendations
-# 4. View Your Manga Recommendations
-# 9. Exit
-#
-# *****=====*****=====*****=====*****=====*****
-# """
+def animenu():
+    menu_selection = int(input(">"))
+    if menu_selection not in [1,2,9]:
+        print("Please input valid selection 1,2 or 9")
+        menu_selection = int(input(">"))
+    elif menu_selection == 1:
+        newrec(AnimeData, "anime")
+        print(Menus.ANIME_MSG)
+    elif menu_selection == 2:
+        viewlist(AnimeData, "Anime \n")
+        print(Menus.RETURN_MSG)
 
 
+print(Menus.MENU_MSG)
 # Main menu loop:
-# while True:
-#   system('clear')
-#   print(MENU_MSG)
-#   menu_selection = int(input(">"))
-#   if menu_selection not in  [1,2,3,4,9]:
-#     print("Please input valid selection 1-9:")
-#     menu_selection = int(input(">"))
-#   elif menu_selection == 1:
-#
-#
-#
-#
-#     break
+while True:
+    menu_selection = int(input(">"))
+    if menu_selection not in [1,2,3,4,9]:
+        print("Please input valid selection 1-4 or 9:")
+        menu_selection = int(input(">"))
+    elif menu_selection == 1:
+        newrec(AnimeData, "anime")
+        print(Menus.ANIME_MSG)
+        animenu()
+    elif menu_selection == 2:
+        newrec(MangaData, "manga")
+        print(Menus.MANGA_MSG)
+    elif menu_selection == 3:
+        viewlist(AnimeData, "Anime \n")
+        print(Menus.RETURN_MSG)
+    elif menu_selection == 4:
+        viewlist(MangaData, "Manga \n")
+        print(Menus.RETURN_MSG)
+    elif menu_selection == 9:
+        break
+
+
---
+++ src/menus.py
@@ -0,0 +1,48 @@
+
+class Menus():
+
+    # Menu Message
+    MENU_MSG = """
+    *****=====*****=====*****=====*****=====*****
+    
+    Would you like to:
+    1. Get New Anime Recommendations
+    2. Get New Manga Recommendations
+    3. View Your Anime Recommendations
+    4. View Your Manga Recommendations
+    9. Exit
+    
+    *****=====*****=====*****=====*****=====*****
+    """
+
+    # Anime Menu
+    ANIME_MSG = """
+    *****=====*****=====*****=====*****=====*****
+    
+    Would you like to:
+    1. Get New Anime Recommendations
+    2. View Your Anime Recommendations
+    9. Exit
+    
+    *****=====*****=====*****=====*****=====*****
+    """
+
+    # Manga Menu
+    MANGA_MSG = """
+    *****=====*****=====*****=====*****=====*****
+    
+    Would you like to:
+    1. Get New Manga Recommendations
+    2. View Your Manga Recommendations
+    9. Exit
+    
+    *****=====*****=====*****=====*****=====*****
+    """
+
+
+    # Return Menu
+    RETURN_MSG = """\n
+    Would you like to:
+    1. Return to Main Menu
+    9. Exit
+    """
